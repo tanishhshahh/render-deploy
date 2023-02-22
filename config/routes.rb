09572ed00cdbb74f199ab03a25a34dda8774ef19
Tanish Shah <tanibshah@gmail.com>
2023-02-22 23:30:06 +0530

    routes file changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
+	get 'about', to: 'pages#about'
+	resources :contacts, only: :create
+	get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
