0026ac0ac92efacbf13c5f6ccf311f38fc3d3572
Tanish Shah <tanibshah@gmail.com>
2023-02-22 23:06:25 +0530

    master key addition

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module RenderDeploy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
   end
 end
